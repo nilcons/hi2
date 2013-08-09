bcf7941960931a79c79da0fbd73a521b8abe995a
Gergely Risko <gergely@risko.hu>
2013-08-09 12:53:16 +0200

    Documentation update

--- README.md
+++ README.md
@@ -15,23 +15,29 @@ can leave out the require line above.
 ## Notable changes to haskell-indentation-mode
 
 * DEL and C-d is not mapped: if you want to indent backwards, you can
-  use S-TAB.
+  use S-TAB.  This means no random jumping on backspace anymore.
 
 * TAB steps to the right as before, but when the end is reached, it
   starts going to the left instead of wrapping around.
 
 * TAB stays inside the code, instead of going to the beginning of the
-  line.
+  line.  As in pyhton-mode and perl-mode.
 
 * Region indentation common case is supported: TAB and S-TAB is simply
-  moving the whole region to the left/right.
+  moving the whole region to the left/right by 1-column.  Can be
+  pressed repeatedly.
 
 * The current indentations are shown as underscores in the current
-  line, so you have some visual indication (can be turned off by
-  setting `hi2-show-indentations` to nil in your init fileor calling
-  `hi2-disable-show-indentations` from the buffer).
+  line.  So you have some visual indication on what's gonna happen.
+  Also useful while hacking on the parser and want to see the results.
+  Can be turned off by setting hi2-show-indentations to nil in your
+  init file or calling hi2-disable-show-indentations from the buffer.
+  If there are collisions with other overlay hacking modes
+  (e.g. fill-column-indicator), try to turn off
+  hi2-show-indentations-after-eol.
 
 * The buffer is not changed when indentation is not changed (so there
-  are no undo points created and no dirty flag in the buffer).
+  are no undo points created and no dirty flag in the buffer if
+  pressing TAB had no effect).
 
 * The code for all this is somewhat commented and cleaned.
--- hi2.el
+++ hi2.el
@@ -29,24 +29,34 @@
 ;; This is a modified version of haskell-indentation-mode.  Currently
 ;; the semantic parser is not changed, but the UI of tab completion is
 ;; reworked.  Most notably:
-;;   - DEL and C-d is not mapped:
-;;       if you want to indent backwards, you can use S-TAB,
-;;   - TAB steps to the right as before, but when the end is reached,
-;;     it starts going to the left instead of wrapping around,
-;;   - TAB stays inside the code, instead of going to the beginning of
-;;     the line,
-;;   - region indentation common case is supported: TAB and S-TAB
-;;     is simply moving the whole region to the left/right,
-;;   - the current indentations are shown as underscores in the
-;;     current line, so you have some visual indication (can be turned
-;;     off by setting `hi2-show-indentations' to nil in your init
-;;     file or calling `hi2-disable-show-indentations' from the
-;;     buffer, if there are issues, also check
-;;     hi2-show-indentations-after-eol),
-;;   - the buffer is not changed when indentation is not changed (so
-;;     there are no undo points created and no dirty flag in the
-;;     buffer),
-;;   - the code for all this is somewhat commented and cleaned.
+
+;;  - DEL and C-d is not mapped: if you want to indent backwards, you can
+;;    use S-TAB.  This means no random jumping on backspace anymore.
+;;
+;;  - TAB steps to the right as before, but when the end is reached, it
+;;    starts going to the left instead of wrapping around.
+;;
+;;  - TAB stays inside the code, instead of going to the beginning of the
+;;    line.  As in pyhton-mode and perl-mode.
+;;
+;;  - Region indentation common case is supported: TAB and S-TAB is simply
+;;    moving the whole region to the left/right by 1-column.  Can be
+;;    pressed repeatedly.
+;;
+;;  - The current indentations are shown as underscores in the current
+;;    line.  So you have some visual indication on what's gonna happen.
+;;    Also useful while hacking on the parser and want to see the results.
+;;    Can be turned off by setting hi2-show-indentations to nil in your
+;;    init file or calling hi2-disable-show-indentations from the buffer.
+;;    If there are collisions with other overlay hacking modes
+;;    (e.g. fill-column-indicator), try to turn off
+;;    hi2-show-indentations-after-eol.
+;;
+;;  - The buffer is not changed when indentation is not changed (so there
+;;    are no undo points created and no dirty flag in the buffer if
+;;    pressing TAB had no effect).
+;;
+;;  - The code for all this is somewhat commented and cleaned.
 
 ;; There is ongoing development on making this better and I invite
 ;; everyone to participate (report bugs, fix issues, etc.):
